6eabe6d0e4ee78fb70a553c297740b61a2b0e105
Vibhu <vibhuedits@gmail.com>
2021-07-02 15:38:29 +0530

    initial commit

---
+++ build/precache-manifest.0da88ca1dbeca2aff195fead99f0f4c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772f1d5e3f6f1206c27a497aafd1d9bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "868e5e2d16c04bb9cfa7",
+    "url": "/static/js/2.9987ebc6.chunk.js"
+  },
+  {
+    "revision": "3dd1dcd5925c7ba87129",
+    "url": "/static/js/main.f0df27dd.chunk.js"
+  },
+  {
+    "revision": "a2ef16d0a5e714453fb7",
+    "url": "/static/js/runtime-main.e1fd096b.js"
+  }
+]);
