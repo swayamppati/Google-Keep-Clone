838bbf973409ab40c0b1586e6729f28d70a0a90b
Vibhu <vibhuedits@gmail.com>
2021-07-02 16:06:23 +0530

    second build

---
+++ build/precache-manifest.1d9c871aff34e51687b03448f18c3d14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25002a28588c5cc6d74a520e3762eeb",
+    "url": "/Google-Keep-Clone/index.html"
+  },
+  {
+    "revision": "868e5e2d16c04bb9cfa7",
+    "url": "/Google-Keep-Clone/static/js/2.9987ebc6.chunk.js"
+  },
+  {
+    "revision": "9da3b660879c3c3bc32a",
+    "url": "/Google-Keep-Clone/static/js/main.d3c98400.chunk.js"
+  },
+  {
+    "revision": "9aef967dab187bbc0222",
+    "url": "/Google-Keep-Clone/static/js/runtime-main.a5c1412d.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0da88ca1dbeca2aff195fead99f0f4c0.js"
+  "/Google-Keep-Clone/precache-manifest.1d9c871aff34e51687b03448f18c3d14.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Google-Keep-Clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
